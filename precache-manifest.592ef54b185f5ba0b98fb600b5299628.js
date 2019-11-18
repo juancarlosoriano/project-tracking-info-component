f211d8a81a2fdf34534b425c085ac8658b9748da
JC Soriano <jc@juancarlosoriano.ca>
2019-11-17 21:17:50 -0500

    Updates

---
+++ precache-manifest.592ef54b185f5ba0b98fb600b5299628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73aac16f67936b25126949434d2269ea",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "2590370ffdbeb2d0f112",
+    "url": "/react-gh-pages/static/css/main.8b18024e.chunk.css"
+  },
+  {
+    "revision": "f948ee0e9ec78948594a",
+    "url": "/react-gh-pages/static/js/2.1eb12c73.chunk.js"
+  },
+  {
+    "revision": "2590370ffdbeb2d0f112",
+    "url": "/react-gh-pages/static/js/main.38025029.chunk.js"
+  },
+  {
+    "revision": "0b63f6f85fa9a87d4e71",
+    "url": "/react-gh-pages/static/js/runtime-main.d44d51f0.js"
+  },
+  {
+    "revision": "39db90b3100f42e4a5c8f99408ffbf2d",
+    "url": "/react-gh-pages/static/media/illustration-devices.39db90b3.svg"
+  },
+  {
+    "revision": "dfc21d9af3253582a8ba91e911f5f7bb",
+    "url": "/react-gh-pages/static/media/logo.dfc21d9a.svg"
+  }
+]);
